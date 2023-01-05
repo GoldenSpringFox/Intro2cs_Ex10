d8c5ee107f02b7d3e01b4f177b8a62822b40c26a
GoldenSpringFox <46161299+GoldenSpringFox@users.noreply.github.com>
2023-01-05 15:16:35 +0200

    fix: blue square border collision

--- src/snake_game.py
+++ src/snake_game.py
@@ -1,3 +1,4 @@
+import game_utils
 from typing import Optional
 from game_display import GameDisplay
 
@@ -14,8 +15,12 @@ class SnakeGame:
     def update_objects(self)-> None:
         if (self.__key_clicked == 'Left') and (self.__x > 0):
             self.__x -= 1
-        elif (self.__key_clicked == 'Right') and (self.__x < 40):
+        elif (self.__key_clicked == 'Right') and (self.__x < game_utils.size.width - 1):
             self.__x += 1
+        elif (self.__key_clicked == 'Down') and (self.__y > 0):
+            self.__y -= 1
+        elif (self.__key_clicked == 'Up') and (self.__y < game_utils.size.height - 1):
+            self.__y += 1
 
     def draw_board(self, gd: GameDisplay) -> None:
         gd.draw_cell(self.__x, self.__y, "blue")
