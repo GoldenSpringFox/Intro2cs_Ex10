6c0587cd0e997a9810de1b07bda213f5ac7817f0
guytrabelsi <guytra2205@gmail.com>
2023-01-09 11:56:40 +0200

    Update snake_game.py

--- src/snake_game.py
+++ src/snake_game.py
@@ -1,13 +1,15 @@
 from typing import Optional, List, Tuple
 from game_display import GameDisplay
-from game_objects.snake import Snake
+#from game_objects.snake import Snake
 from game_objects.apple_handler import AppleHandler
 from game_objects.wall_handler import WallHandler
+import Snake
+
 
 class SnakeGame:
 
     def __init__(self, rounds: int) -> None:
-        self.__snake = Snake()
+        self.__snake = Snake.Snake(20, 20)
         self.__apple_handler = AppleHandler()
         self.__wall_handler = WallHandler()
         self.__key_clicked = None
@@ -15,29 +17,31 @@ class SnakeGame:
         self.rounds = rounds
 
     def is_cell_empty(self, x: int, y: int):
-        return (x,y) not in (
-            self.__snake.get_snake_coordinates() + 
-            self.__apple_handler.get_apples_coordinates() +
-            self.__wall_handler.get_walls_coordinates())
-    
+        return (x, y) not in (
+                self.__snake.get_snake_coordinates() +
+                self.__apple_handler.get_apples_coordinates() +
+                self.__wall_handler.get_walls_coordinates())
+
     def are_cells_empty(self, cells: List[Tuple[int, int]]):
         return all(self.is_cell_empty(*cell) for cell in cells)
 
-    def read_key(self, key_clicked: Optional[str])-> None:
+    def read_key(self, key_clicked: Optional[str]) -> None:
         self.__key_clicked = key_clicked
 
-    def update_objects(self)-> None:
+    def update_objects(self) -> None:
         self.__snake.move(self.__key_clicked)
 
     def draw_board(self, gd: GameDisplay) -> None:
-        gd.draw_cell(*self.__snake.get_snake_coordinates()[0], "blue")
+        for cord in self.__snake.get_snake_coordinates():
+            gd.draw_cell(*cord, 'black')
+        #gd.draw_cell(*self.__snake.get_snake_coordinates()[0], "black")
 
     def end_round(self) -> None:
         self.rounds -= 1
 
     def is_over(self) -> bool:
         return self.rounds == 0
-    
+
     @property
     def score(self):
         return self.__score
