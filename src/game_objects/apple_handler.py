6124586a4543d9441341a38df354e0fd51969173
guytrabelsi <guytra2205@gmail.com>
2023-01-10 22:01:33 +0200

    Update apple_handler.py

--- src/game_objects/apple_handler.py
+++ src/game_objects/apple_handler.py
@@ -1,12 +1,15 @@
-from src import game_utils
 from typing import List, Tuple
+
 
 class AppleHandler:
     def __init__(self) -> None:
         self.__apples: List[Tuple[int, int]] = []
-    
+
     def generate_new_apple(self, cord):
         self.__apples.append(cord)
-    
+
     def get_apples_coordinates(self) -> List[Tuple[int, int]]:
         return self.__apples
+
+    def remove_apple(self, apple):
+        self.__apples.remove(apple)
