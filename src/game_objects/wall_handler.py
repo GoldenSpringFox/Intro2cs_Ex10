adfe4e0272923661d75a1e6f7c477c2c1a6deab9
GoldenSpringFox <46161299+GoldenSpringFox@users.noreply.github.com>
2023-01-11 18:43:46 +0200

    wall_handler refactor
    shortened code + wrote tests

--- src/game_objects/wall_handler.py
+++ src/game_objects/wall_handler.py
@@ -2,37 +2,29 @@ from typing import List, Tuple
 
 
 class WallHandler:
-    DIRECTION_DICT = {'Up': [(0, 1), (0, -1)], 'Down': [(0, 1), (0, -1)], 'Right': [(1, 0), (-1, 0)],
-                      'Left': [(1, 0), (-1, 0)]}
     MOVEMENT_DICT = {'Up': (0, 1), 'Down': (0, -1), "Left": (1, 0), 'Right': (1, 0)}
 
-    def __init__(self) -> None:
+    def __init__(self, length=3) -> None:
         self.__walls: List[Tuple[int, int, str]] = []
+        self.__length = length
 
-    def generate_new_wall(self, wall):
+    def add_wall(self, wall: Tuple[int, int, str]):
         self.__walls.append(wall)
 
     def move_wall(self):
-        lst = []
-        for wall in self.__walls:
-            wall_cord = (wall[0], wall[1])
-            _wall = self._adding_tuples(wall_cord, self.MOVEMENT_DICT[wall[2]])
-            moved_wall = (_wall[0], _wall[1], wall[2])
-            lst.append(moved_wall)
-        self.__walls = lst
-    def _adding_tuples(self, wall_mid_cord: Tuple[int, int], cord: Tuple[int, int]):
-        new_wall_cord = wall_mid_cord[0] + cord[0], wall_mid_cord[1] + cord[1]
-        return new_wall_cord
+        for _ in range(self.num_of_walls):
+            x, y, direction = self.__walls.pop(0)
+            x, y = (x + self.MOVEMENT_DICT[direction][0], y + self.MOVEMENT_DICT[direction][1])
+            self.__walls.append((x, y, direction))
 
     @property
     def num_of_walls(self):
         return len(self.__walls)
 
+    def _get_wall_coordinates(self, x, y, direction) -> List[Tuple[int, int]]:
+        return [(x + self.MOVEMENT_DICT[direction][1] * i, y + self.MOVEMENT_DICT[direction][0] * i)
+                for i in range(-(self.__length//2), self.__length//2 + 1)]
+            
+
     def get_walls_coordinates(self) -> List[Tuple[int, int]]:
-        lst = []
-        for wall in self.__walls:
-            wall_cord = (wall[0], wall[1])
-            lst.append(wall_cord)
-            for cord in self.DIRECTION_DICT[wall[2]]:
-                lst.append(self._adding_tuples(wall_cord, cord))
-        return lst
+        return [coordinate for wall in self.__walls for coordinate in self._get_wall_coordinates(*wall)]
